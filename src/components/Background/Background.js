7714bf8dbbc7bc0686b66ae5b97c99ca729e9f04
aereeeee <dofl5576@gmail.com>
2019-06-27 16:21:18 +0900

    Add images

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Background.scss';
-import image1 from '../../assets/img/img1.jpg'
-import image2 from '../../assets/img/img2.jpg'
-import image3 from '../../assets/img/img3.jpg'
+import image1 from '../../assets/img/img1.png'
+import image2 from '../../assets/img/img2.png'
+import image3 from '../../assets/img/img3.png'
+import image4 from '../../assets/img/img4.png'
+import image5 from '../../assets/img/img5.png'
+import image6 from '../../assets/img/img6.png'
+import image7 from '../../assets/img/img7.png'
+import image8 from '../../assets/img/img8.png'
 
 const Background = () => {
     return (
@@ -10,11 +15,11 @@ const Background = () => {
             <img src={image1} data='box0' alt="" className="backimg"/>
             <img src={image2} data='box1' alt="" className="backimg"/>
             <img src={image3} data='box2' alt="" className="backimg"/>
-            <img src={image1} data='box3' alt="" className="backimg"/>
-            <img src={image2} data='box4' alt="" className="backimg"/>
-            <img src={image3} data='box5' alt="" className="backimg"/>
-            <img src={image1} data='box6' alt="" className="backimg"/>
-            <img src={image2} data='box7' alt="" className="backimg"/>
+            <img src={image4} data='box3' alt="" className="backimg"/>
+            <img src={image5} data='box4' alt="" className="backimg"/>
+            <img src={image6} data='box5' alt="" className="backimg"/>
+            <img src={image7} data='box6' alt="" className="backimg"/>
+            <img src={image8} data='box7' alt="" className="backimg"/>
         </div>
     );
 };
