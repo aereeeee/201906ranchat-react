4c66796c493eb8594c14a6e14dcc0940505935d9
aereeeee <dofl5576@gmail.com>
2019-06-28 18:06:52 +0900

    중간점검

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './Background.scss';
-import image1 from '../../assets/img/1.png'
-import image2 from '../../assets/img/2.png'
-import image3 from '../../assets/img/3.png'
-import image4 from '../../assets/img/4.png'
-import image5 from '../../assets/img/5.png'
-import image6 from '../../assets/img/6.png'
-import image7 from '../../assets/img/6.png'
-import image8 from '../../assets/img/6.png'
+import intro from '../../assets/img/thum.png'
+import image1 from '../../assets/img/a1.png'
+import image2 from '../../assets/img/a2.png'
+import image3 from '../../assets/img/a3.png'
+import image4 from '../../assets/img/a4.png'
+import image5 from '../../assets/img/a5.png'
+import image6 from '../../assets/img/a6.png'
+import image7 from '../../assets/img/a7.png'
+// import image8 from '../../assets/img/a7.png'
 
 const Background = () => {
     return (
         <div className='background'>
-            <img src={image1} data='box0' alt="" className="backimg"/>
-            <img src={image2} data='box1' alt="" className="backimg"/>
-            <img src={image3} data='box2' alt="" className="backimg"/>
-            <img src={image4} data='box3' alt="" className="backimg"/>
-            <img src={image5} data='box4' alt="" className="backimg"/>
-            <img src={image6} data='box5' alt="" className="backimg"/>
-            <img src={image7} data='box6' alt="" className="backimg"/>
-            <img src={image8} data='box7' alt="" className="backimg"/>
+            <img src={intro} data='box0' alt="" className="backimg"/>
+            <img src={image1} data='box1' alt="" className="backimg"/>
+            <img src={image1} data='box2' alt="" className="backimg"/>
+            <img src={image2} data='box3' alt="" className="backimg"/>
+            <img src={image3} data='box4' alt="" className="backimg"/>
+            <img src={image4} data='box5' alt="" className="backimg"/>
+            <img src={image5} data='box6' alt="" className="backimg"/>
+            <img src={image6} data='box7' alt="" className="backimg"/>
+            <img src={image7} data='box8' alt="" className="backimg"/>
+            
         </div>
     );
 };
